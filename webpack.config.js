d33a073e1ba6f8f194474b71191e17c5c0720ce5
Marina Drugina <druginamarina@gmail.com>
2023-02-09 18:57:57 +0300

    update files

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCSSExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+  entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.join(__dirname, 'dist'),
+      assetModuleFilename: path.join('img', '[name].[contenthash][ext]'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCSSExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new MiniCSSExtractPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCSSExtractPlugin(),
-  ],
 };
